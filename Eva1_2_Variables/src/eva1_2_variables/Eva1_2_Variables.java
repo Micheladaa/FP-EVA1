91791e4c9de653eba9b09e3d6b1f82848f3ba651
Micheladaa <L24550253@chihuahua2.tecnm.mx>
2024-08-23 17:54:07 -0600

    Add files via upload

---
+++ Eva1_2_Variables/src/eva1_2_variables/Eva1_2_Variables.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_2_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva1_2_Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                                            //Tipos de datos
+        System.out.println("HOLA MUNDO!!"); //Cadena de texto
+        System.out.println(100);            //Entero
+        System.out.println(3.1416);         //Número con punto flotante
+        System.out.println('c');            //Un caracter
+        System.out.println(true);           //Valores lógicos(booleanos)
+        System.out.println(
+                           false
+                                )
+                                ;   //;--> FIN DE INSTRUCCIÓN
+    
+    /*ENTEROS:
+      int
+      long
+      short
+     *NÚMEROS CON PARTE DECIMAL (FLOTANTES):
+      float
+      double
+     *CADENA DE TEXTO:
+      String
+     *CARACTERES:
+      char
+     *VALORES LÓGICOS:
+      boolean*/
+        
+        
+    /*SINTAXIS PARA DECLARAR VARIABLES:
+     -TIPO_DE_DATO NOMBRE_DE_LA_VARIABLE
+    */
+    String nombre;
+    String apellido;
+    String curp;
+    float promedio;
+      }
+}
+
+//Michelle Flores
